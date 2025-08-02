79ccc67c7ab7c6104f2b633a3a3f9a26ae067794
abhisheksinghvi09 <abhisheksinghvi9otc@gmail.com>
2025-08-02 11:17:02 +0000

    Revert "."
    This reverts commit 32a780fdef7804f86249f72de16fcf822804972d.

--- backend/cmd/tools/tools.go
+++ backend/cmd/tools/tools.go
@@ -2,6 +2,4 @@ package tools
 
 import (
 	_ "github.com/abhisheksinghvi09/go-boilerplate/internal/config"
-	_ "github.com/abhisheksinghvi09/go-boilerplate/internal/database"
-	_ "github.com/abhisheksinghvi09/go-boilerplate/internal/logger"
 )
