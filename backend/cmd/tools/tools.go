32a780fdef7804f86249f72de16fcf822804972d
abhisheksinghvi09 <abhisheksinghvi9otc@gmail.com>
2025-08-02 11:12:00 +0000

    .

--- backend/cmd/tools/tools.go
+++ backend/cmd/tools/tools.go
@@ -2,4 +2,6 @@ package tools
 
 import (
 	_ "github.com/abhisheksinghvi09/go-boilerplate/internal/config"
+	_ "github.com/abhisheksinghvi09/go-boilerplate/internal/database"
+	_ "github.com/abhisheksinghvi09/go-boilerplate/internal/logger"
 )
